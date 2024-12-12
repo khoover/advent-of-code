6ac9fa456749efe0c3369600c877836bd631e964
Ken Hoover <k.hoover3@gmail.com>
2024-12-12 01:47:05 -0800

    Done day 12

---
+++ 2024/src/day12.rs
@@ -0,0 +1,235 @@
+use super::*;
+use partitions::{partition_vec, PartitionVec};
+
+#[aoc(day12, part1)]
+pub fn part1(s: &str) -> u64 {
+    let columns = s.find("\n").unwrap();
+    let stride = columns + 1;
+    let rows = (s.len() + 1) / stride;
+    let input = s.as_bytes();
+
+    let mut regions: PartitionVec<u64> = PartitionVec::with_capacity(input.len());
+    regions.resize(columns, 1);
+
+    {
+        // First row
+        regions[0] = 2;
+        for i in 0..columns - 1 {
+            if input[i] != input[i + stride] {
+                regions.push(1);
+                regions[i] += 1;
+            } else {
+                regions.push(0);
+                regions.union(i, i + columns);
+            }
+            if input[i] != input[i + 1] {
+                regions[i] += 1;
+                regions[i + 1] += 1;
+            } else {
+                regions.union(i, i + 1);
+            }
+        }
+        regions[columns - 1] += 1;
+        if input[columns - 1] != input[columns - 1 + stride] {
+            regions.push(1);
+            regions[columns - 1] += 1;
+        } else {
+            regions.push(0);
+            regions.union(columns - 1, columns * 2 - 1);
+        }
+    }
+
+    {
+        // Middle rows
+        for row in 1..rows - 1 {
+            let input_base = row * stride;
+            let region_base = row * columns;
+            regions[region_base] += 1; // left edge of grid
+            for i in 0..columns - 1 {
+                // Assumption: left and top edges have been accounted of.
+                if input[i + input_base] != input[i + input_base + stride] {
+                    regions.push(1);
+                    regions[i + region_base] += 1;
+                } else {
+                    regions.push(0);
+                    regions.union(region_base + i, region_base + i + columns);
+                }
+                if input[input_base + i] != input[input_base + i + 1] {
+                    regions[region_base + i] += 1;
+                    regions[region_base + i + 1] += 1;
+                } else {
+                    regions.union(region_base + i, region_base + i + 1);
+                }
+            }
+            regions[region_base + columns - 1] += 1;
+            if input[input_base + columns - 1] != input[input_base + columns - 1 + stride] {
+                regions.push(1);
+                regions[region_base + columns - 1] += 1;
+            } else {
+                regions.push(0);
+                regions.union(region_base + columns - 1, region_base + 2 * columns - 1);
+            }
+        }
+    }
+
+    {
+        // Final row
+        let input_base = (rows - 1) * stride;
+        let region_base = (rows - 1) * columns;
+        regions[region_base] += 1; // left edge
+        for i in 0..columns - 1 {
+            regions[region_base + i] += 1; // bottom edge
+            if input[input_base + i] != input[input_base + i + 1] {
+                regions[region_base + i] += 1;
+                regions[region_base + i + 1] += 1;
+            } else {
+                regions.union(region_base + i, region_base + i + 1);
+            }
+        }
+        regions[region_base + columns - 1] += 2;
+    }
+
+    regions
+        .all_sets()
+        .map(|set| {
+            let (area, perimeter) = set.fold((0, 0), |(area, perimeter), (idx, edges)| {
+                let input_idx = idx + (idx / columns);
+                debug!(idx);
+                debug!(edges);
+                (area + 1, perimeter + edges)
+            });
+            debug!(area);
+            debug!(perimeter);
+            area * perimeter
+        })
+        .sum()
+}
+
+#[aoc(day12, part2)]
+pub fn part2(s: &str) -> u64 {
+    let columns = s.find("\n").unwrap();
+    let stride = columns + 1;
+    let rows = (s.len() + 1) / stride;
+    let input = s.as_bytes();
+
+    let mut regions: PartitionVec<()> = PartitionVec::with_capacity(input.len()); // is now count of corners at space
+    regions.resize(columns, ());
+
+    {
+        // First row
+        for i in 0..columns - 1 {
+            regions.push(());
+            if input[i] == input[i + stride] {
+                regions.union(i, i + columns);
+            }
+            if input[i] == input[i + 1] {
+                regions.union(i, i + 1);
+            }
+        }
+        regions.push(());
+        if input[columns - 1] == input[columns - 1 + stride] {
+            regions.union(columns - 1, columns * 2 - 1);
+        }
+    }
+
+    {
+        // Middle rows
+        for row in 1..rows - 1 {
+            let input_base = row * stride;
+            let region_base = row * columns;
+            for i in 0..columns - 1 {
+                regions.push(());
+                // Assumption: left and top edges have been accounted of.
+                if input[i + input_base] == input[i + input_base + stride] {
+                    regions.union(region_base + i, region_base + i + columns);
+                }
+                if input[input_base + i] == input[input_base + i + 1] {
+                    regions.union(region_base + i, region_base + i + 1);
+                }
+            }
+            regions.push(());
+            if input[input_base + columns - 1] == input[input_base + columns - 1 + stride] {
+                regions.union(region_base + columns - 1, region_base + 2 * columns - 1);
+            }
+        }
+    }
+
+    {
+        // Final row
+        let input_base = (rows - 1) * stride;
+        let region_base = (rows - 1) * columns;
+        for i in 0..columns - 1 {
+            if input[input_base + i] == input[input_base + i + 1] {
+                regions.union(region_base + i, region_base + i + 1);
+            }
+        }
+    }
+
+    regions
+        .all_sets()
+        .map(|set| {
+            let (area, perimeter) = set.fold((0, 0), |(area, perimeter), (idx, _)| {
+                let neighbour_offsets = [
+                    -(columns as isize) - 1,
+                    -(columns as isize),
+                    -(columns as isize) + 1,
+                    -1,
+                    1,
+                    columns as isize - 1,
+                    columns as isize,
+                    columns as isize + 1,
+                ];
+                let neighbour_different = neighbour_offsets
+                    .into_iter()
+                    .map(|offset| {
+                        idx.checked_add_signed(offset)
+                            .filter(|neighbour_idx| *neighbour_idx < regions.len())
+                            .map(|neighbour_idx| regions.other_sets(idx, neighbour_idx))
+                            .unwrap_or(true)
+                    })
+                    .fold(0_u8, |acc, is_different| {
+                        (acc << 1) | if is_different { 1 } else { 0 }
+                    });
+
+                let mut edges = match (neighbour_different & 0b01011010).count_ones() {
+                    4 => 4,
+                    3 => 2,
+                    2 if (neighbour_different & 0b01000010).count_ones() == 1 => 1,
+                    _ => 0,
+                };
+                edges += (neighbour_different & 0b11010000 == 0b10000000) as u64;
+                edges += (neighbour_different & 0b01101000 == 0b00100000) as u64;
+                edges += (neighbour_different & 0b00010110 == 0b00000100) as u64;
+                edges += (neighbour_different & 0b00001011 == 0b00000001) as u64;
+
+                (area + 1, perimeter + edges)
+            });
+            debug!(area);
+            debug!(perimeter);
+            area * perimeter
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1_simple() {
+        let s = "AAAA
+BBCD
+BBCC
+EEEC";
+        assert_eq!(part1(s), 140);
+    }
+
+    #[test]
+    fn test_part2_simple() {
+        let s = "AAAA
+BBCD
+BBCC
+EEEC";
+        assert_eq!(part2(s), 80);
+    }
+}
