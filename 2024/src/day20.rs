3dddacf186d95d960241206ce19ad8a4ff7b22c5
Ken Hoover <k.hoover3@gmail.com>
2024-12-20 10:28:16 -0800

    Rayon is an instant speed boost

--- 2024/src/day20.rs
+++ 2024/src/day20.rs
@@ -19,13 +19,14 @@ pub fn part1(s: &str) -> usize {
         for offset in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
             let new_pos_row = pos.0.checked_add_signed(offset.0).filter(|x| *x < rows);
             let new_pos_col = pos.1.checked_add_signed(offset.1).filter(|y| *y < columns);
-            if let Some(new_pos) = new_pos_row.zip(new_pos_col) {
-                if new_pos != last_pos && bytes[new_pos.0 * stride + new_pos.1] != b'#' {
-                    path.push(new_pos);
-                    last_pos = pos;
-                    pos = new_pos;
-                    continue 'outer;
-                }
+            let Some(new_pos) = new_pos_row.zip(new_pos_col) else {
+                continue;
+            };
+            if new_pos != last_pos && bytes[new_pos.0 * stride + new_pos.1] != b'#' {
+                path.push(new_pos);
+                last_pos = pos;
+                pos = new_pos;
+                continue 'outer;
             }
         }
         panic!("Uh oh");
@@ -39,7 +40,7 @@ pub fn part1(s: &str) -> usize {
         .collect::<FxHashMap<_, _>>();
 
     let mut count = 0;
-    for (i, pos) in path.iter().copied().enumerate() {
+    for (i, pos) in path.into_iter().enumerate() {
         for offset in [
             (0, 2),
             (0, -2),
@@ -80,13 +81,14 @@ pub fn part2(s: &str) -> usize {
         for offset in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
             let new_pos_row = pos.0.checked_add_signed(offset.0).filter(|x| *x < rows);
             let new_pos_col = pos.1.checked_add_signed(offset.1).filter(|y| *y < columns);
-            if let Some(new_pos) = new_pos_row.zip(new_pos_col) {
-                if new_pos != last_pos && bytes[new_pos.0 * stride + new_pos.1] != b'#' {
-                    path.push(new_pos);
-                    last_pos = pos;
-                    pos = new_pos;
-                    continue 'outer;
-                }
+            let Some(new_pos) = new_pos_row.zip(new_pos_col) else {
+                continue;
+            };
+            if new_pos != last_pos && bytes[new_pos.0 * stride + new_pos.1] != b'#' {
+                path.push(new_pos);
+                last_pos = pos;
+                pos = new_pos;
+                continue 'outer;
             }
         }
         panic!("Uh oh");
@@ -103,20 +105,23 @@ pub fn part2(s: &str) -> usize {
         .enumerate()
         .map(|(i, pos)| {
             let mut count = 0;
-            for x_offset in -20_isize..=20 {
-                let y_allowable = 20 - x_offset.abs();
-                for y_offset in -y_allowable..=y_allowable {
-                    let new_pos_row = pos.0.checked_add_signed(x_offset);
-                    let new_pos_col = pos.1.checked_add_signed(y_offset);
-                    if let Some(distance) = new_pos_row
-                        .zip(new_pos_col)
-                        .and_then(|new_pos| distance_map.get(&new_pos))
-                    {
-                        if i + 100 + x_offset.unsigned_abs() + y_offset.unsigned_abs() <= *distance
-                        {
-                            count += 1;
-                        }
-                    }
+            let min_offset = -(pos.0.min(20) as isize);
+            let max_offset = (rows - pos.0).min(20) as isize;
+            for x_offset in min_offset..=max_offset {
+                let abs_offset = x_offset.unsigned_abs();
+                let new_pos_row = unsafe { pos.0.checked_add_signed(x_offset).unwrap_unchecked() };
+                let y_allowable = 20 - abs_offset;
+                let min_offset = -(pos.1.min(y_allowable) as isize);
+                let max_offset = (columns - pos.1).min(y_allowable) as isize;
+                for y_offset in min_offset..=max_offset {
+                    let new_pos_col =
+                        unsafe { pos.1.checked_add_signed(y_offset).unwrap_unchecked() };
+                    count += distance_map
+                        .get(&(new_pos_row, new_pos_col))
+                        .copied()
+                        .is_some_and(|distance| {
+                            i + 100 + abs_offset + y_offset.unsigned_abs() <= distance
+                        }) as usize;
                 }
             }
             count
