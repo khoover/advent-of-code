cbab65324438066fdad5c59e1c0cec85eb5b4c06
Ken Hoover <ken.hoover@instacart.com>
2025-12-01 11:20:52 -0800

    Finish up part 2

--- 2025/src/bin/day1.rs
+++ 2025/src/bin/day1.rs
@@ -20,13 +20,43 @@ fn part1(s: &str) -> Result<u64> {
     Ok(count)
 }
 
-pub fn main() -> Result<()> {
-    run_day(part1)
+fn part2(s: &str) -> Result<u64> {
+    let mut acc = 50_i16;
+    let mut count = 0_u64;
+    for l in s.lines() {
+        let bytes = l.as_bytes();
+        let coeff: i16 = match bytes[0] {
+            b'R' => 1,
+            b'L' => -1,
+            _ => unreachable!(),
+        };
+        let mut num: u64 = unsafe { str::from_utf8_unchecked(&bytes[1..]) }.parse()?;
+        if num >= 100 {
+            count += num / 100;
+            num %= 100;
+        }
+        let old_acc = acc;
+        acc += num as i16 * coeff;
+        if acc >= 100 {
+            count += 1;
+            acc -= 100;
+        } else if acc < 0 {
+            if old_acc != 0 {
+                count += 1;
+            }
+            acc += 100;
+        } else if acc == 0 {
+            count += 1;
+        }
+    }
+    Ok(count)
 }
 
-#[test]
-fn example_part1() {
-    const INPUT: &str = "L68
+pub fn main() -> Result<()> {
+    run_day(part1, part2)
+}
+
+const INPUT: &str = "L68
 L30
 R48
 L5
@@ -36,5 +66,13 @@ L1
 L99
 R14
 L82";
+
+#[test]
+fn example_part1() {
     assert_eq!(part1(INPUT).unwrap(), 3);
+}
+
+#[test]
+fn example_part2() {
+    assert_eq!(part2(INPUT).unwrap(), 6);
 }
