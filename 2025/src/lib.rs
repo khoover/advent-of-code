0567d5f8e4d33f1b04dd6cc89a9fa5558136d881
Ken Hoover <ken.hoover@instacart.com>
2025-12-01 11:21:08 -0800

    Add an option to decide whether to run part 1 or part 2

--- 2025/src/lib.rs
+++ 2025/src/lib.rs
@@ -1,22 +1,39 @@
 use std::{fmt::Display, fs::read_to_string, path::PathBuf};
 
 use anyhow::{Context, Result};
-use clap::{Arg, Command, value_parser};
+use clap::{Arg, ArgAction, Command, value_parser};
 
-pub fn run_day<F, R>(f: F) -> Result<()>
+pub fn run_day<F1, R1, F2, R2>(part1: F1, part2: F2) -> Result<()>
 where
-    R: Display,
-    F: for<'a> FnOnce(&'a str) -> Result<R>,
+    R1: Display,
+    F1: for<'a> FnOnce(&'a str) -> Result<R1>,
+    R2: Display,
+    F2: for<'a> FnOnce(&'a str) -> Result<R2>,
 {
     let matches = Command::new("AOC Runner")
-        .arg(Arg::new("input_path").value_parser(value_parser!(PathBuf)))
+        .arg(
+            Arg::new("input_path")
+                .value_parser(value_parser!(PathBuf))
+                .required(true),
+        )
+        .arg(
+            Arg::new("part_two")
+                .long("part_two")
+                .action(ArgAction::SetTrue),
+        )
         .get_matches();
     let path = matches
         .get_one::<PathBuf>("input_path")
         .context("Failed to parse path from command invocation")?;
     let s =
         read_to_string(path).with_context(|| format!("Failed to read from {}", path.display()))?;
-    f(&s).map(|r| {
-        println!("{}", r);
-    })
+    if matches.get_flag("part_two") {
+        part2(&s).map(|r| {
+            println!("{}", r);
+        })
+    } else {
+        part1(&s).map(|r| {
+            println!("{}", r);
+        })
+    }
 }
