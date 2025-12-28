a8cba1928195f9a0aa296c5a9a99f7bec87536b1
Ken Hoover <ken.hoover@instacart.com>
2025-12-27 18:51:55 -0800

    Use the byte grid for day 4

--- 2025/src/bin/day4.rs
+++ 2025/src/bin/day4.rs
@@ -1,50 +1,22 @@
 use std::collections::VecDeque;
 
 use anyhow::Result;
+use aoc_2025::byte_grid::Grid;
 use aoc_2025::run_day;
 
 fn part1(s: &str) -> Result<u64> {
-    let lines: Vec<_> = s.trim().lines().map(|l| l.as_bytes()).collect();
-    let height = lines.len();
-    let width = lines[0].len();
-    let mut acc: u64 = 0;
-    for row in 0..height {
-        for col in 0..width {
-            if lines[row][col] == b'@'
-                && grid_iter(row, col, width, height)
-                    .map(|(row, col)| if lines[row][col] == b'@' { 1 } else { 0 })
-                    .sum::<u8>()
-                    < 4
-            {
-                acc += 1;
-            }
-        }
-    }
-    Ok(acc)
-}
-
-const OFFSETS: [(isize, isize); 8] = [
-    (-1, -1),
-    (0, -1),
-    (1, -1),
-    (-1, 0),
-    (1, 0),
-    (-1, 1),
-    (0, 1),
-    (1, 1),
-];
-
-fn grid_iter(
-    row: usize,
-    col: usize,
-    width: usize,
-    height: usize,
-) -> impl Iterator<Item = (usize, usize)> {
-    OFFSETS.into_iter().filter_map(move |(a, b)| {
-        let row = row.checked_add_signed(a).filter(|&x| x < height);
-        let col = col.checked_add_signed(b).filter(|&x| x < width);
-        row.zip(col)
-    })
+    let grid = Grid::from_input_str(s.trim())?;
+    Ok(grid
+        .enumerate()
+        .filter(|(_, b)| **b == b'@')
+        .filter(|&((row, col), _)| {
+            grid.neighbours(row, col)
+                .map(|coord| grid[coord])
+                .filter(|&x| x == b'@')
+                .count()
+                < 4
+        })
+        .count() as u64)
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -60,48 +32,31 @@ impl TileState {
 }
 
 fn part2(s: &str) -> Result<u64> {
-    let lines: Vec<_> = s.trim().lines().map(|l| l.as_bytes()).collect();
-    let height = lines.len();
-    let width = lines[0].len();
-    let to_linear = |row: usize, col: usize| -> usize { row * height + col };
-
-    let mut state: Vec<TileState> = lines
-        .iter()
-        .copied()
-        .enumerate()
-        .flat_map(|(row, l)| {
-            let line_ref = &lines;
-            l.iter()
-                .copied()
-                .enumerate()
-                .map(move |(col, b)| TileState {
-                    has_wall: b == b'@',
-                    neighbour_wall_count: grid_iter(row, col, width, height)
-                        .filter(|&(row, col)| line_ref[row][col] == b'@')
-                        .count() as u8,
-                })
-        })
-        .collect();
-
+    let grid = Grid::from_input_str(s.trim())?;
     let mut to_flip: VecDeque<(usize, usize)> = VecDeque::new();
-    for row in 0..height {
-        for col in 0..width {
-            if state[to_linear(row, col)].is_flippable() {
-                to_flip.push_back((row, col));
-            }
+    let mut state = grid.map(|(row, col), &b| {
+        let state = TileState {
+            has_wall: b == b'@',
+            neighbour_wall_count: grid
+                .neighbours(row, col)
+                .filter(|&coord| grid[coord] == b'@')
+                .count() as u8,
+        };
+        if state.is_flippable() {
+            to_flip.push_back((row, col));
         }
-    }
+        state
+    });
 
     let mut acc: u64 = 0;
     while let Some((row, col)) = to_flip.pop_front() {
-        let idx = to_linear(row, col);
-        if state[idx].is_flippable() {
+        if state[(row, col)].is_flippable() {
             acc += 1;
-            state[idx].has_wall = false;
-            to_flip.extend(grid_iter(row, col, width, height).filter(|&(row, col)| {
-                let idx = to_linear(row, col);
-                state[idx].neighbour_wall_count -= 1;
-                state[idx].has_wall && state[idx].neighbour_wall_count == 3
+            state[(row, col)].has_wall = false;
+            to_flip.extend(state.neighbours(row, col).filter(|&coord| {
+                let neighbour = &mut state[coord];
+                neighbour.neighbour_wall_count -= 1;
+                neighbour.has_wall && neighbour.neighbour_wall_count == 3
             }));
         }
     }
--- 2025/src/byte_grid.rs
+++ 2025/src/byte_grid.rs
@@ -4,13 +4,13 @@ use anyhow::{Result, anyhow};
 use memchr::memchr;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Grid {
-    data: Vec<u8>,
+pub struct Grid<T> {
+    data: Vec<T>,
     height: usize,
     width: usize,
 }
 
-impl Grid {
+impl Grid<u8> {
     pub fn from_input_str(s: &str) -> Result<Self> {
         Grid::from_input_lines(s.lines())
     }
@@ -34,7 +34,22 @@ impl Grid {
             width,
         })
     }
+    pub fn find(&self, val: u8) -> Option<(usize, usize)> {
+        memchr(val, self.data.as_slice()).map(|idx| (idx / self.width, idx % self.width))
+    }
+}
 
+impl<T: Clone> Grid<T> {
+    pub fn new(width: usize, height: usize, fill: T) -> Self {
+        Grid {
+            data: vec![fill; width * height],
+            width,
+            height,
+        }
+    }
+}
+
+impl<T> Grid<T> {
     pub fn width(&self) -> usize {
         self.width
     }
@@ -43,59 +58,97 @@ impl Grid {
         self.height
     }
 
-    pub fn get(&self, row: usize, col: usize) -> Option<&u8> {
+    pub fn get(&self, row: usize, col: usize) -> Option<&T> {
         if col >= self.width {
             return None;
         }
         self.data.get(row * self.width + col)
     }
 
-    pub fn get_mut(&mut self, row: usize, col: usize) -> Option<&mut u8> {
+    pub fn get_mut(&mut self, row: usize, col: usize) -> Option<&mut T> {
         if col >= self.width {
             return None;
         }
         self.data.get_mut(row * self.width + col)
     }
 
-    pub fn col(&self, idx: usize) -> impl Iterator<Item = u8> + '_ {
+    pub fn enumerate(&self) -> impl DoubleEndedIterator<Item = ((usize, usize), &T)> {
+        self.data
+            .iter()
+            .enumerate()
+            .map(|(i, x)| ((i / self.width, i % self.width), x))
+    }
+
+    pub fn neighbours(
+        &self,
+        row: usize,
+        col: usize,
+    ) -> impl DoubleEndedIterator<Item = (usize, usize)> + 'static {
+        const OFFSETS: [(isize, isize); 8] = [
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+            (-1, 0),
+            (1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+        ];
+        let height = self.height;
+        let width = self.width;
+
+        OFFSETS.into_iter().filter_map(move |(a, b)| {
+            let row = row.checked_add_signed(a).filter(|&x| x < height);
+            let col = col.checked_add_signed(b).filter(|&x| x < width);
+            row.zip(col)
+        })
+    }
+
+    pub fn col(&self, idx: usize) -> impl Iterator<Item = &T> {
         self.col_at_rows(idx, 0..self.height)
     }
 
-    pub fn col_at_rows<'a, I, II>(
-        &'a self,
-        col_idx: usize,
-        rows: II,
-    ) -> impl Iterator<Item = u8> + 'a
+    pub fn col_at_rows<I, II>(&self, col_idx: usize, rows: II) -> impl Iterator<Item = &T>
     where
-        I: Iterator<Item = usize> + 'a,
+        I: Iterator<Item = usize>,
         II: IntoIterator<IntoIter = I>,
     {
-        rows.into_iter().map(move |row| self[(row, col_idx)])
+        rows.into_iter().map(move |row| &self[(row, col_idx)])
     }
 
-    pub fn find(&self, val: u8) -> Option<(usize, usize)> {
-        memchr(val, self.data.as_slice()).map(|idx| (idx / self.width, idx % self.width))
+    pub fn map<U>(&self, mut f: impl FnMut((usize, usize), &T) -> U) -> Grid<U> {
+        Grid {
+            width: self.width,
+            height: self.height,
+            data: self.enumerate().map(|(coord, x)| f(coord, x)).collect(),
+        }
     }
 }
 
-impl Index<usize> for Grid {
-    type Output = [u8];
+impl<T> Index<usize> for Grid<T> {
+    type Output = [T];
 
     fn index(&self, index: usize) -> &Self::Output {
         &self.data[index * self.width..(index + 1) * self.width]
     }
 }
 
-impl IndexMut<usize> for Grid {
-    fn index_mut(&mut self, index: usize) -> &mut [u8] {
+impl<T> IndexMut<usize> for Grid<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.data[index * self.width..(index + 1) * self.width]
     }
 }
 
-impl Index<(usize, usize)> for Grid {
-    type Output = u8;
+impl<T> Index<(usize, usize)> for Grid<T> {
+    type Output = T;
 
-    fn index(&self, index: (usize, usize)) -> &u8 {
+    fn index(&self, index: (usize, usize)) -> &T {
         self.get(index.0, index.1).unwrap()
+    }
+}
+
+impl<T> IndexMut<(usize, usize)> for Grid<T> {
+    fn index_mut(&mut self, (row, col): (usize, usize)) -> &mut T {
+        self.get_mut(row, col).unwrap()
     }
 }
